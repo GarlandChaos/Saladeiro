e6439e9922fefedef97d328688815b7d37a0bc09
Bernardo Alvarez Braga <bernardo.alvarezbraga@gmail.com>
2021-09-15 13:44:22 -0300

    Improved Players Controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    Rigidbody rigidbody = null;
+
+    void ClearRigidbodyValues()
+    {
+        rigidbody.velocity = Vector3.zero;
+        rigidbody.angularVelocity = Vector3.zero;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        ClearRigidbodyValues();
+    }
+
+    private void OnCollisionExit(Collision collision)
+    {
+        ClearRigidbodyValues();
+    }
+}
--- Assets/Scripts/PlayersController.cs
+++ Assets/Scripts/PlayersController.cs
@@ -8,8 +8,8 @@ public class PlayersController : MonoBehaviour
     Transform transformPlayer1 = null;
     [SerializeField]
     Transform transformPlayer2 = null;
-    [SerializeField]
-    Camera cam = null;
+    //[SerializeField]
+    //Camera cam = null;
     float speed = 3f;
     bool swap =  false;
     [SerializeField]
@@ -18,6 +18,7 @@ public class PlayersController : MonoBehaviour
     Animator animator = null;
     int WalkHash = Animator.StringToHash("Walk");
     int WaveHash = Animator.StringToHash("Wave");
+    Coroutine followPlayerCoroutine = null;
 
     private void Awake()
     {
@@ -38,8 +39,7 @@ public class PlayersController : MonoBehaviour
     {
         if (!swap)
         {
-            MovePlayer1();
-            FollowPlayer1();
+            MovePlayer();
             if (Input.GetKeyDown(KeyCode.Space))
             {
                 SwapPlayers();
@@ -60,102 +60,82 @@ public class PlayersController : MonoBehaviour
 
     }
 
-    void MovePlayer1()
+    void MovePlayer()
     {
-        //não funciona em pc remoto...
-        //float horizontal = Input.GetAxis("Mouse X");
-        //Debug.Log(horizontal);
-        //transformPlayer1.Rotate(horizontal * 30f * Vector3.up, Space.World);
-
-        if (Input.GetKey(KeyCode.Q))
-        {
-            transformPlayer1.Rotate(Vector3.down * 30f * Time.deltaTime);
-        }
-        else if (Input.GetKey(KeyCode.E))
-        {
-            transformPlayer1.Rotate(Vector3.up * 30f * Time.deltaTime);
-        }
-
         Vector3 move = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
         if(move != Vector3.zero)
         {
             transformPlayer1.forward = move;
             animator.SetBool(WalkHash, true);
             transformPlayer1.position += move * speed * Time.deltaTime;
+            if(followPlayerCoroutine != null)
+            {
+                StopCoroutine(followPlayerCoroutine);
+            }
+            followPlayerCoroutine = StartCoroutine(FollowPlayerCoroutine());
         }
         else
         {
             animator.SetBool(WalkHash, false);
         }
-
-        //if (Input.GetKey(KeyCode.W))
-        //{
-        //    animator.SetBool(WalkHash, true);
-        //    transformPlayer1.position += transformPlayer1.forward * speed * Time.deltaTime;
-        //}
-        //else if (Input.GetKey(KeyCode.S))
-        //{
-        //    animator.SetBool(WalkHash, true);
-        //    Vector3 direction = transformPlayer1.position - transformPlayer1.forward;
-        //    //transformPlayer1.rotation = Quaternion.LookRotation(direction, Vector3.up);
-        //    transform.forward = direction;
-        //    transformPlayer1.position -= transformPlayer1.forward * speed * Time.deltaTime;
-        //}
-        //else if (Input.GetKey(KeyCode.A))
-        //{
-        //    animator.SetBool(WalkHash, true);
-        //    transformPlayer1.position -= transformPlayer1.right * speed * Time.deltaTime;
-        //}
-        //else if (Input.GetKey(KeyCode.D))
-        //{
-        //    animator.SetBool(WalkHash, true);
-        //    transformPlayer1.position += transformPlayer1.right * speed * Time.deltaTime;
-        //}
-
-        //if(Input.GetKeyUp(KeyCode.W) || Input.GetKeyUp(KeyCode.A) || Input.GetKeyUp(KeyCode.S) || Input.GetKeyUp(KeyCode.D))
-        //{
-        //    animator.SetBool(WalkHash, false);
-        //}
     }
 
-    void FollowPlayer1()
+    IEnumerator FollowPlayerCoroutine()
     {
-        transformPlayer2.LookAt(transformPlayer1);
+        WaitForEndOfFrame wait = new WaitForEndOfFrame();
+        AnimationCurve curve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+        float timer = 0f;
+        float duration = 0.2f;
+        Vector3 startPos = transformPlayer2.position;
+        Vector3 player1Pos = transformPlayer1.position;
+        player1Pos.y = transformPlayer2.position.y;
+        Vector3 direction = Vector3.Normalize(player1Pos - startPos);
+        Vector3 endPos = player1Pos - direction * 1.5f;
+        Vector3 delta = endPos - startPos;
+        transformPlayer2.LookAt(player1Pos);
 
-        if (Vector3.Distance(transformPlayer1.position, transformPlayer2.position) > 4f)
+        do
         {
-            transformPlayer2.position += Vector3.Normalize(transformPlayer1.position - transformPlayer2.position) * speed * Time.deltaTime;
+            timer += Time.deltaTime / duration;
+            transformPlayer2.position = startPos + delta * curve.Evaluate(timer);
+            yield return wait;
         }
+        while (timer < 1f);
+
+        followPlayerCoroutine = null;
     }
 
-    public async void SwapPlayers()
+    public void SwapPlayers()
     {
         swap = true;
+        if (followPlayerCoroutine != null)
+        {
+            StopCoroutine(followPlayerCoroutine);
+        }
         transformPlayer2.localRotation = new Quaternion(0, 0, 0, transformPlayer1.rotation.w);
-        await SwapCamera();
-        //Destroy(transformPlayer1.GetComponent<PlayerInteraction>());
+        //await SwapCamera();
         transformPlayer1.GetComponent<Interactor>().enabled = false;
         Transform p = transformPlayer1;
         transformPlayer1 = transformPlayer2;
         transformPlayer2 = p;
         transformPlayer1.GetComponent<Interactor>().enabled = true;
-        cam.transform.SetParent(transformPlayer1);
+        //cam.transform.SetParent(transformPlayer1);
         swap = false;
     }
 
-    IEnumerator SwapCamera()
-    {
-        float timer = 0f;
-        float animationTime = 2f;
-        AnimationCurve curve = AnimationCurve.EaseInOut(0, 0, 1, 1);
-        Vector3 startPos = cam.transform.position;
-        Vector3 endPos = transformPlayer2.position + Vector3.back * Vector3.Distance(transformPlayer1.position, startPos) + new Vector3(0, startPos.y, 0);
-        do
-        {
-            timer += Time.deltaTime / animationTime;
-            cam.transform.position = Vector3.Lerp(startPos, endPos, curve.Evaluate(timer));
-            yield return new WaitForEndOfFrame();
-        }
-        while (timer < 1f);
-    }
+    //IEnumerator SwapCamera()
+    //{
+    //    float timer = 0f;
+    //    float animationTime = 2f;
+    //    AnimationCurve curve = AnimationCurve.EaseInOut(0, 0, 1, 1);
+    //    Vector3 startPos = cam.transform.position;
+    //    Vector3 endPos = transformPlayer2.position + Vector3.back * Vector3.Distance(transformPlayer1.position, startPos) + new Vector3(0, startPos.y, 0);
+    //    do
+    //    {
+    //        timer += Time.deltaTime / animationTime;
+    //        cam.transform.position = Vector3.Lerp(startPos, endPos, curve.Evaluate(timer));
+    //        yield return new WaitForEndOfFrame();
+    //    }
+    //    while (timer < 1f);
+    //}
 }
